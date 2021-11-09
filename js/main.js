991e7802bac077b86e0509cd9cb0c22cd567be45
Ivan Tsvetkov <tsvetkov-vanechka@inbox.ru>
2021-11-09 22:04:28 +0300

    Первая версия

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".menu__nav");
+
+burger.addEventListener("click",function(){
+    menu.classList.add("active");
+})
