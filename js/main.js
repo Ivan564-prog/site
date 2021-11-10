ae98c3d93c4bb00638855a7f4db1d5e2ea4de304
Ivan Tsvetkov <tsvetkov-vanechka@inbox.ru>
2021-11-10 23:10:28 +0300

    Третья версия

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-const burger = document.querySelector(".header__burger");
-const menu = document.querySelector(".menu__nav");
-
-burger.addEventListener("click",function(){
-    menu.classList.toggle("active");
-})
+$(".header__burger").click(function(){
+    $(".menu__nav").addClass("active");
+});
+$(".close__burger,.main__information").click(function(){
+    $(".menu__nav").removeClass("active");
+});
+$(".slider__projects").slick({
+    prevArrow: '<div class="prev"><svg><use xlink:href="img/sprite.svg#prev"></use></svg> Пред</div>',
+    nextArrow: '<div class="next">След <svg><use xlink:href="img/sprite.svg#next"></use></svg></div>',
+});
