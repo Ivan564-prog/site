d878ee2caf2d16fa1081cb7a3e80e298ab2bd38b
Ivan Tsvetkov <tsvetkov-vanechka@inbox.ru>
2021-11-16 22:49:38 +0300

    5 версия

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$(window).scroll(function (){
+    const scroll=$(window).scrollTop();
+    console.log(scroll);
+    if(scroll>=45){
+        $(".header").addClass('scroll');
+    }
+    else if(scroll<45)
+        $(".header").removeClass('scroll');
+    if(scroll>=1400){
+        $(".staging__item").addClass('active');
+    }
+    else if(scroll<=917)
+        $(".staging__item").removeClass('active');
+});
 $(".header__burger").click(function(){
     $(".menu__nav").addClass("active");
 });
